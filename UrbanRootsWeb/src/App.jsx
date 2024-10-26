a9b14bc866dddf0a03d43c7b76dc0e1eab8192e6
GS <sathwikag8@gmail.com>
2024-10-26 13:43:06 +0530

    resolved all merge conflicts

--- UrbanRootsWeb/src/App.jsx
+++ UrbanRootsWeb/src/App.jsx
@@ -1,21 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './Components/signupComponent/Signup'
 import './Components/signupComponent/Signup.css'
-<<<<<<< HEAD
-=======
 import Login from './Components/LoginComponent/login'
 import './Components/LoginComponent/login.css'
->>>>>>> origin
 import LandingPage from './Components/landingPageComponent/LandingPage'
 import Events from './Components/eventsComponent/Events'
 import Discussions from './Components/discussionComponent/Discussions'
 import Resources from './Components/resourceComponent/Resources'
 import About from './Components/aboutComponent/About'
-<<<<<<< HEAD
-=======
 import Profile from './Components/profileComponent/profile'
 import './Components/profileComponent/profile.css'
->>>>>>> origin
 import Blog from './Components/blogComponent/Blog'
 function App() {
  
@@ -24,20 +18,14 @@ function App() {
      <BrowserRouter>
       <Routes>
        <Route path="/signup" element={<Signup/>}></Route>
-<<<<<<< HEAD
-=======
        <Route path="/login" element={<Login/>}></Route>
->>>>>>> origin
        <Route path="/landingPage" element={<LandingPage/>}></Route>
         <Route path="/events" element={<Events/>}></Route>
        <Route path="/discussions" element={<Discussions/>}></Route>
        <Route path="/resources" element={<Resources/>}></Route>
        <Route path="/resources/:id" Component={Blog}/>
        <Route path="/about" element={<About/>}></Route>
-<<<<<<< HEAD
-=======
        <Route path="/profile" element={<Profile/>}></Route>
->>>>>>> origin
       </Routes>
      </BrowserRouter>
  
